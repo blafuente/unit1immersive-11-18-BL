3da53a549a7dff970232fee323df650073f09f80
Brian Lafuente <loulafuente@MacBook-Pro-2.local>
2018-11-13 17:18:46 -0500

    guess the number game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,32 @@
-#Guessing game for numbers between 1 to 10
+# #Guessing game for numbers between 1 to 10
 print "Welcome to the number guessing game"
 print "I am thinking of a number between 1 and 10"
 
-number = raw_input("Number?: ")
-secretNumber = 5
+# import random
+# n = 5 #random.randint(1, 11)
+# guess = int(raw_input("Guess the number: "))
+# while n != "guess":
+#     print
+#     if guess < n:
+#         print "guess is low"
+#         guess = int(raw_input("Guess again: "))
+#     elif guess > n:
+#         print "guess is high"
+#         guess = int(raw_input("Guess again: "))
+#     else:
+#         print "you guessed it!"
+#         break
+#     print
 
-while (number <= 10):
-    if (number == secretNumber):
-        print "You guessed the right correct number!" + str(number)
+secret_number = 5
+guess = int(raw_input("What's the number? "))
+while secret_number != "guess":
+    if guess < secret_number:
+        print "%d is too low. Guess again." % guess
+        guess = int(raw_input("What's the number? "))
+    elif guess > secret_number:
+        print "%d is too high. Guess again." % guess
+        guess = int(raw_input("What's the number? "))
     else:
-        print "Wrong number, try again."
-
-
+        print "Yes! You win!"
+        break

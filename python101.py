e30a5c3604a40a08dae7da39a33e524a93357bda
Brian Lafuente <loulafuente@MacBook-Pro-2.local>
2018-11-13 14:28:13 -0500

    Add operators

--- python101.py
+++ python101.py
@@ -1,47 +1,83 @@
-print ('Hello World') ;
-print 'Hello, World'  ;
+# print ('Hello World') ;
+# print 'Hello, World'  ;
 
-print """
-    It was a dark and stormy night.
-    A murder happened. 
-"""
+# print """
+#     It was a dark and stormy night.
+#     A murder happened. 
+# """
 
-name = raw_input("Please enter your first name: ")
-print "Hello " + name
+# name = raw_input("Please enter your first name: ")
+# print "Hello " + name
 
-# Variables
-# - strings, letters, numbers, or any other stuff 
-# you can make with a keyboard
-# a Variables is just a fasy way to refer to something else
-# variables do not make the program faster.
-# They make the program slower!
-# Variables make it easier for us to write programs. 
+# # Variables
+# # - strings, letters, numbers, or any other stuff 
+# # you can make with a keyboard
+# # a Variables is just a fasy way to refer to something else
+# # variables do not make the program faster.
+# # They make the program slower!
+# # Variables make it easier for us to write programs. 
 
-theBestClass = "the 11-18 immersive"
-print theBestClass
+# theBestClass = "the 11-18 immersive"
+# print theBestClass
 
-# Data types:
-#     - Programming languages see different types of variables differently 
-#     - string - English stuff. 
-#     - Number - any integer and for example 2.2e10
-#     - - Floats and integers
-#     - Floats = has a decimal within the value
-#     Booleans - true or false, on or off, 1 or 0, yes or no, right or left
-#     list - list of things
-#     dictionaries - variable of variables
-#     object - super dictionaries
+# # Data types:
+# #     - Programming languages see different types of variables differently 
+# #     - string - English stuff. 
+# #     - Number - any integer and for example 2.2e10
+# #     - - Floats and integers
+# #     - Floats = has a decimal within the value
+# #     Booleans - true or false, on or off, 1 or 0, yes or no, right or left
+# #     list - list of things
+# #     dictionaries - variable of variables
+# #     object - super dictionaries
 
-# Primitive Data types = string, number, boolean
+# # Primitive Data types = string, number, boolean
 
-month = "November";
-print type(month);
-date = 13
-print type(date)
-dateAsFloat = 13.0
-print type(dateAsFloat)
-aBool = True
-print type(aBool)
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# month = "November";
+# print type(month);
+# date = 13
+# print type(date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
+# aBool = True
+# print type(aBool)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
+
+# # concatenate is progromming speak for add things together
+# first = "Brian"
+# last = "Lafuente"
+# fullName = first + last;
+# fullName = first + " " + last;
+# print fullName
+
+# fourteen = 10 + 4
+# print fourteen
+# fourteen = "10" + "4"
+# print fourteen
+# # fourteen = 10 + "4"
+# # print fourteen
+
+# # cast = change a variable to a new data type
+# fourteen = int("10") + 4
+# print fourteen
+
+# Math = +, -, /, *, %
+# print 2+2
+# print 2-2
+# print 2/2
+# print 2*2
+# % = Modulus, Modulus divides the number and gives you the remainder
+# print 2%2
+# print 2**3
+# print 10**87
+# A string and a * and a number = give me X strings
+print "-" * 20
+
+# Python does not have a simple incremementer
+num = 1;
+# num++
+num += 1
+

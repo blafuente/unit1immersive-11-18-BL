066c751a22c6d47febafc53bcfa9cffb05012d18
Brian Lafuente <loulafuente@MacBook-Pro-2.local>
2018-11-13 12:03:22 -0500

    First commit of the class

---
+++ python101.py
@@ -0,0 +1,34 @@
+print ('Hello World') ;
+print 'Hello, World'  ;
+
+print """
+    It was a dark and stormy night.
+    A murder happened. 
+"""
+
+name = raw_input("Please enter your first name: ")
+print "Hello " + name
+
+# Variables
+# - strings, letters, numbers, or any other stuff 
+# you can make with a keyboard
+# a Variables is just a fasy way to refer to something else
+# variables do not make the program faster.
+# They make the program slower!
+# Variables make it easier for us to write programs. 
+
+theBestClass = "the 11-18 immersive"
+print theBestClass
+
+# Data types:
+#     - Programming languages see different types of variables differently 
+#     - string - English stuff. 
+#     - Number - any integer and for example 2.2e10
+#     - - Floats and integers
+#     - Floats = has a decimal within the value
+#     Booleans - true or false, on or off, 1 or 0, yes or no, right or left
+#     list - list of things
+#     dictionaries - variable of variables
+#     object - super dictionaries
+
+# Primitive Data types = string, number, boolean

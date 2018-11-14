3086bf0fe7edcbcd13fab09d535d4d0f37ff8ef2
Brian Lafuente <loulafuente@MacBook-Pro-2.local>
2018-11-14 09:38:11 -0500

    Added Bonus play again option

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,15 +5,22 @@ def playOn():
     print "Welcome to the number guessing game"
     print "I am thinking of a number between 1 and 10"
 
-    import random
+    import random 
     secret_number = random.randint(1,10);
-    print secret_number
+    print secret_number #printing out secret number to test out when guessed correctly.
     number_of_guesses = 5;
     guess = int(raw_input("What's the number? "))
     while secret_number != "guess":
         #When user runs out of guesses
         if number_of_guesses == 0:    
             print "You're out of guesses"
+            #Asks if you want to continue the game
+            answer = raw_input("Do you want to play again? [Y/N] ")
+            if answer == "Y":
+                return playOn()
+            elif answer == "N":
+                print "Good Bye"
+                pass
             break
         #User's guess is low    
         if guess < secret_number:
@@ -30,14 +37,21 @@ def playOn():
         #User's guess is correct!
         elif guess == secret_number:
             print "Yes! You win!"
-            #gameOn(answer);
+            #Asks user if you want to play again.
+            answer = raw_input("Do you want to play again? [Y/N] ")
+            if answer == "Y":
+                return playOn()
+            elif answer == "N":
+                print "Good Bye"
+                pass
             break
 
-# def gameOn(answer):
-#     answer = raw_input("Do you want to play again? [Y/N] ")
-#     if answer == "Y":
-#     elif answer == "N":
-#         print "good bye"
-#         break
+#  def gameOn(answer):
+#      answer = raw_input("Do you want to play again? [Y/N] ")
+#      if answer == "Y":
+#          playOn()
+#      elif answer == "N":
+#          print "good bye"
+#          break
   
 playOn()   

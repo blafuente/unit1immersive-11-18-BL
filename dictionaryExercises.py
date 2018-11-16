7f58a755c83856bda5aa920bda1fdf13d0c63026
Brian Lafuente <loulafuente@MacBook-Pro-3.local>
2018-11-16 09:04:46 -0500

    Dictionary Exercise 11/15

--- dictionaryExercises.py
+++ dictionaryExercises.py
@@ -6,10 +6,42 @@ phonebook_dict = {
     'Elizabeth': '484-584-2923'
 }
 
+# Printing Elizabeth's number
 print phonebook_dict['Elizabeth']
-phonebook_dict.append({
-    'Kareem': '938-489-1234'
-})
+# Add Kareem's number to phonebook dictionary
+phonebook_dict.update({'Kareem': '938-489-1234'})
+# Deleting Alice's number
 del phonebook_dict['Alice']
+# Changing Bob's number in the phonboook
 phonebook_dict['Bob'] = '968-345-3245'
-print phonebook_dict[]
+#printing out the numbers in the phonebook dictionary
+print phonebook_dict
+print
+
+#Exercise 2
+ramit = ({
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+})
+
+# Write a python expression that gets the email address of Ramit
+print ramit['email']
+# Write a python expression that gets the first of Ramit's interests
+print ramit['interests'][0]
+# Wrtie a python expresssion that gets the email addresss of Jasmine
+print ramit['friends'][0]['email']
+# Wrtie a python expression that gets the second of Jan's two interests. 
+print ramit['friends'][1]['interests'][1]

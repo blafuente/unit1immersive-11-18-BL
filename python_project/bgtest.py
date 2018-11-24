878edcda00f705cfc46a117031f4c4227e1ab428
Brian Lafuente <loulafuente@MacBook-Pro-5.attlocal.net>
2018-11-24 14:41:10 -0500

    testing side scrolling

---
+++ python_project/bgtest.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.locals import *
+import os
+import sys
+import math 
+
+pygame.init()
+
+screen_size = (960, 1080)
+pygame_screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Testing Background")
+
+background_image = pygame.image.load('bg_test.png')
+bgx = 0
+bgx2 = background_image.get_width()
+clock = pygame.time.Clock()
+
+speed = 30 
+game_on = True
+while game_on:
+    #This draws the background image onto the screen
+    pygame_screen.blit(background_image,[bgx,0])
+    pygame_screen.blit(background_image,[bgx2, 0])
+    # pygame.display.flip()
+    pygame.display.update()
+
+    clock.tick(speed) #FPS
+    # Moving background image back
+    bgx -= 1.4  
+    bgx2 -= 1.4
+
+    #Conditional
+    # First background image will start at (0,0) and start to move backwards
+    #
+    if bgx < background_image.get_width() * -1:
+        bgx = background_image.get_width()
+        # This resets the image when it reaches it end
+    # Brings back the game so it runs at the end of the first image    
+    if bgx2 < background_image.get_width() * -1:
+        bgx2 = background_image.get_width()  
+
+    #To be able to exit out the game 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # THE USER CLICKED THE RED DOT!
+            #These aren't the droids were lookin for. quit
+            game_on = False
+    
+    
